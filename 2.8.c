28afdaababe7b9b56fe058bfcf0631b6042b956d
midinka <dinam03777@gmail.com>
2022-10-20 19:28:12 +0300

    add 6, 7, 8

---
+++ 2.8.c
@@ -0,0 +1,30 @@
+//функция дается изначально
+//уравнение функции передается в функцию как параметр
+//найдем корень на интервале [0;1] уравнения x^3-3*x+1=0 с точностью 10^-3
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+double foo(double l, double r, double e){
+    double c;
+    if (pow(l,3) - 3*l + 1 == 0) return l;
+    if (pow(r,3) - 3*r + 1 == 0) return r;
+    while ((r - l) > e){
+        c = (l + r) / 2.;
+        if (pow(c,3) - 3.*c + 1. < 0.){
+            r = c;
+        }else {
+            l = c;
+        }
+    }
+    printf("%lf", (l + r) / 2.);
+}
+
+void main(){
+    double l = 0.;
+    double r = 1.;
+    double e = 0.001;
+    foo(l, r, e);
+}
